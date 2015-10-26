54bac4b71d29ab5f2d87f123a16614bf07945fd3
tapangoel1994 <tapangoel1994@gmail.com>
2015-10-26 11:39:58 +0530

    Convert to 2 species model.
    All functions other than saving files and plotting have been updated to
    incorporate 2 species.
    Testing has to be done.

--- model.cpp
+++ model.cpp
@@ -1,4 +1,5 @@
-//This code simulates the original Viscek Model as is in the 1995 PRL - no repulsion, no turning angle, scalar noise and angle based implementation
+//This code simulates the mixed species Viscek Model - no repulsion, no turning angle, scalar noise and angle based implementation.
+
 // Angles are chosen over [-PI,PI] and are maintained so throughout.
 // The size of spheres in the animations are not to scale.
 #include<stdio.h>
@@ -12,21 +13,21 @@ using namespace std;
 #define PI 3.14159
 
 
-const int N=100;
+const int N=100; // Number of individuals in each flock
 
 
-float L,r=1,v=.03,delta_t=1,eta;
+float L,r=1,delta_r,v=.03,delta_t=1,eta;
 
 
-float simulate(float x[2][N],float y[2][N],float theta[2][N],long int T, FILE *fp); //Runs simulation
-void initialize(float x[2][N],float y[2][N],float theta[2][N]); // Gives random initial values to position and direction of each particle
-void update_pos(float x[2][N],float y[2][N],float theta[2][N]); // Updates position of each particle
-void update_vel(float x[2][N],float y[2][N],float theta[2][N]); // Updates velocity of each particle
+float simulate(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N],long int T, FILE *fp); //Runs simulation
+void initialize(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N]); // Gives random initial values to position and direction of each particle
+void update_pos(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N]); // Updates position of each particle
+void update_vel(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N]); // Updates velocity of each particle
 float distance(float x1,float x2,float y1,float y2); // calculates distance along torus between two points
 float arctan(float sin,float cos); //generates the angle between [-PI PI] for given sin and cos values
 float limit(float x); // limits angle to [-PI PI] for some angle x
-void swap(float x[2][N],float y[2][N],float theta[2][N]); // x[0][i] = x[1][i] and so for other variables
-float Orderparameter(float theta[2][N]); 
+void swap(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N]); // x[0][i] = x[1][i] and so for other variables
+float Orderparameter(float theta1[2][N],float theta2[2][N]); 
 float *correlation(float x[2][N],float y[2][N],float theta[2][N],float l,float delta_l);
 
 
@@ -38,7 +39,7 @@ void write_to_file(float x[2][N],float y[2][N]);
 
 int main()
 {
-     float x[2][N],y[2][N],theta[2][N];
+     float x1[2][N],y1[2][N],theta1[2][N],x2[2][N],y2[2][N],theta2[2][N];
      int n=1;
      float v_a[n],mean,stdev;
      float density;
@@ -57,7 +58,7 @@ int main()
 	  printf("%s\n",name);
 	  data = fopen(name,"w");
 	  
-	  v_a[0] = simulate(x,y,theta,T,data); 
+	  v_a[0] = simulate(x1,y1,theta1,x2,y2,theta2,T,data); 
 	  
 	  fclose(data);
 	  
@@ -68,31 +69,31 @@ int main()
 }
 
 
-float simulate(float x[2][N],float y[2][N],float theta[2][N],long int T,FILE *fp)
+float simulate(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N],long int T,FILE *fp)
 {
      
      // FILE *gnupipe;
      //  gnupipe = popen("gnuplot -persistent","w");
      
      
-     initialize(x,y,theta);   
+     initialize(x1,y1,theta1,x2,y2,theta2);   
      long int t;
      int i;
      for(t=0;t<T;t++)
      {
-	  update_pos(x,y,theta);
-	  update_vel(x,y,theta);
+	  update_pos(x1,y1,theta1,x2,y2,theta2);
+	  update_vel(x1,y1,theta1,x2,y2,theta2);
 	  
 	  // fprintf(fp,"%ld\t%f\n",t,Orderparameter(theta));
 	  // write_to_file(x,y);
 	  
 	  //plot(gnupipe);
 	  
-	  swap(x,y,theta);
+	  swap(x1,y1,theta1,x2,y2,theta2);
      }
      
      // pclose(gnupipe);
-     
+     /*
      for(i = 0;i < (N-1);i++)
      {
 	  fprintf(fp,"x%d,y%d,theta%d,",i,i,i); 
@@ -115,75 +116,115 @@ float simulate(float x[2][N],float y[2][N],float theta[2][N],long int T,FILE *fp
 	  
 	  
 	  swap(x,y,theta);
-     }
+     }*/
      
      
-     return (Orderparameter(theta));
+     return (Orderparameter(theta1,theta2));
 }
 
 
-void initialize(float x[2][N],float y[2][N],float theta[2][N])
+void initialize(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N])
 {
      srand(time(NULL));
      
      for(int i=0;i<N;i++)
      {
-	  x[0][i]= ( (float)rand()/(float)RAND_MAX )*L;
-	  y[0][i]= ( ((float)rand())/((float)RAND_MAX) )*L;
-	  theta[0][i] = -PI + ( ((float)rand())/ ((float)RAND_MAX) )*2*PI;   
+	  x1[0][i]= ( (float)rand()/(float)RAND_MAX )*L;
+	  y1[0][i]= ( ((float)rand())/((float)RAND_MAX) )*L;
+	  theta1[0][i] = -PI + ( ((float)rand())/ ((float)RAND_MAX) )*2*PI;   
+	  
+	  x2[0][i]= ( (float)rand()/(float)RAND_MAX )*L;
+	  y2[0][i]= ( ((float)rand())/((float)RAND_MAX) )*L;
+	  theta2[0][i] = -PI + ( ((float)rand())/ ((float)RAND_MAX) )*2*PI;  
      }
 }
 
-void update_pos(float x[2][N],float y[2][N],float theta[2][N])
+void update_pos(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N])
 {
      for(int i=0;i<N;i++)
      {
-	  x[1][i] = x[0][i] + v*cos(theta[0][i])*delta_t;
-	  y[1][i] = y[0][i] + v*sin(theta[0][i])*delta_t;
+	  x1[1][i] = x1[0][i] + v*cos(theta1[0][i])*delta_t;
+	  y1[1][i] = y1[0][i] + v*sin(theta1[0][i])*delta_t;
 	  
-	  if(x[1][i] > L)
+	  x2[1][i] = x2[0][i] + v*cos(theta2[0][i])*delta_t;
+	  y2[1][i] = y2[0][i] + v*sin(theta2[0][i])*delta_t;
+	  
+	  if(x1[1][i] > L)
 	  {
-	       x[1][i] -= L;
+	       x1[1][i] -= L;
 	  }
 	  
-	  if(x[1][i] < 0)
+	  if(x1[1][i] < 0)
 	  {
-	       x[1][i] += L;
+	       x1[1][i] += L;
 	  }
 	  
-	  if(y[1][i] > L)
+	  if(y1[1][i] > L)
 	  {
-	       y[1][i]-= L;
+	       y1[1][i]-= L;
 	  }
 	  
-	  if(y[1][i] < 0)
+	  if(y1[1][i] < 0)
 	  {
-	       y[1][i] += L;
+	       y1[1][i] += L;
+	  }
+	  
+	  if(x2[1][i] > L)
+	  {
+	       x2[1][i] -= L;
+	  }
+	  
+	  if(x2[1][i] < 0)
+	  {
+	       x2[1][i] += L;
+	  }
+	  
+	  if(y2[1][i] > L)
+	  {
+	       y2[1][i]-= L;
+	  }
+	  
+	  if(y2[1][i] < 0)
+	  {
+	       y2[1][i] += L;
 	  }
 	  
      }  
 }
-void update_vel(float x[2][N],float y[2][N],float theta[2][N])
+void update_vel(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N])
 {
      for(int i=0; i<N;i++)
      {
-	  float sumsin;
-	  float sumcos;
-	  float count;
-	  float d;
-	  sumsin = 0;
-	  sumcos = 0;
-	  count = 0;
+	  float sumsin1,sumsin2;
+	  float sumcos1,sumcos2;
+	  float count1,count2;
+	  float d_same,d_diff;
+	  sumsin1 = 0;
+	  sumcos1 = 0;
+	  count1 = 0;
+	  
+	  sumsin1 = 0;
+	  sumcos1 = 0;
+	  count1 = 0;
 	  
 	  for(int j =0 ; j < N; j++)
 	  {
-	       d = distance(x[0][i],x[0][j],y[0][i],y[0][j]);
+	       d_same = distance(x1[0][i],x1[0][j],y1[0][i],y1[0][j]);
 	       
-	       if(d < r)
+	       if(d_same < r)
 	       {
-		    sumsin += sin( theta[0][j]);
-		    sumcos += cos( theta[0][j]);
-		    count += 1;
+		    sumsin1 += sin( theta1[0][j]);
+		    sumcos1 += cos( theta1[0][j]);
+		    count1 += 1;
+	       }
+	       
+	       d_diff = distance(x1[0][i],x2[0][j],y1[0][i],y2[0][j]);
+	       
+	       if(d_diff < (r-delta_r))
+	       {
+		    sumsin2 += sin( theta2[0][j]);
+		    sumcos2 += cos( theta2[0][j]);
+		    count2 += 1;
 	       }
 	  }
 	  
@@ -192,34 +233,86 @@ void update_vel(float x[2][N],float y[2][N],float theta[2][N])
 	  float thetaavg;
 	  float delta_theta; 
 	  
-	  if(count == 0) {
-	       count = 1;
-	       sumsin = sin(theta[0][i]);
-	       sumcos = cos(theta[0][i]);
-	  }
-	  
-	  sinavg = sumsin/count;
-	  cosavg = sumcos/count;
+	 
+	  sinavg = (sumsin1+sumsin2)/(count1 + count2);
+	  cosavg = (sumcos1 + sumcos2)/(count1 + count2);
 	  thetaavg = arctan(sinavg,cosavg); 
 	  delta_theta = eta*( (float)rand()/(float)RAND_MAX ) - eta/2;
 	  
-	  theta[1][i] = thetaavg + delta_theta;
+	  theta1[1][i] = thetaavg + delta_theta;
 	  
-	  theta[1][i] = limit(theta[1][i]);
+	  theta1[1][i] = limit(theta1[1][i]);
      }
+     
+     
+     for( int i=0; i<N;i++)
+     {
+	  float sumsin1,sumsin2;
+	  float sumcos1,sumcos2;
+	  float count1,count2;
+	  float d_same,d_diff;
+	  sumsin1 = 0;
+	  sumcos1 = 0;
+	  count1 = 0;
+	  
+	  sumsin1 = 0;
+	  sumcos1 = 0;
+	  count1 = 0;
+	  
+	  for(int j =0 ; j < N; j++)
+	  {
+	       d_same = distance(x2[0][i],x2[0][j],y2[0][i],y2[0][j]);
+	       
+	       if(d_same < r)
+	       {
+		    sumsin1 += sin( theta2[0][j]);
+		    sumcos1 += cos( theta2[0][j]);
+		    count1 += 1;
+	       }
+	       
+	       d_diff = distance(x2[0][i],x1[0][j],y2[0][i],y1[0][j]);
+	       
+	       if(d_diff < (r-delta_r))
+	       {
+		    sumsin2 += sin( theta1[0][j]);
+		    sumcos2 += cos( theta1[0][j]);
+		    count2 += 1;
+	       }
+	  }
+	  
+	  float sinavg;
+	  float cosavg;
+	  float thetaavg;
+	  float delta_theta; 
+	  
+	  
+	  sinavg = (sumsin1+sumsin2)/(count1 + count2);
+	  cosavg = (sumcos1 + sumcos2)/(count1 + count2);
+	  thetaavg = arctan(sinavg,cosavg); 
+	  delta_theta = eta*( (float)rand()/(float)RAND_MAX ) - eta/2;
+	  
+	  theta2[1][i] = thetaavg + delta_theta;
+	  
+	  theta2[1][i] = limit(theta2[1][i]);
+     }
+
 }
 
-void swap(float x[2][N],float y[2][N],float theta[2][N])
+void swap(float x1[2][N],float y1[2][N],float theta1[2][N],float x2[2][N],float y2[2][N],float theta2[2][N])
 {
      for(int i=0;i<N;i++)
      {
-	  x[0][i] = x[1][i];
-	  y[0][i] = y[1][i];
-	  theta[0][i] = theta[1][i]; 
+	  x1[0][i] = x1[1][i];
+	  y1[0][i] = y1[1][i];
+	  theta1[0][i] = theta1[1][i]; 
+	  
+	  x2[0][i] = x2[1][i];
+	  y2[0][i] = y2[1][i];
+	  theta2[0][i] = theta2[1][i]; 
      }
 }
 
-float Orderparameter(float theta[2][N])
+float Orderparameter(float theta1[2][N],float theta2[2][N])
 {
      float sumcos,sumsin;
      sumcos =0;
@@ -227,11 +320,11 @@ float Orderparameter(float theta[2][N])
      
      for(int i=0;i<N;i++)
      {
-	  sumsin += sin(theta[1][i]);
-	  sumcos += cos(theta[1][i]);
+	  sumsin += sin(theta1[1][i]) + sin(theta2[1][i]);
+	  sumcos += cos(theta1[1][i]) + cos(theta2[1][i]) ;
      }
      
-     return((sqrt(sumsin*sumsin + sumcos*sumcos))/N);
+     return((sqrt(sumsin*sumsin + sumcos*sumcos))/(2*N));
 }
 
 float distance(float x1,float x2,float y1,float y2)
